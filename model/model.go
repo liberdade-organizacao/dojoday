d6c286b2c33f25f440ab7a4688240d9b387fe125
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-08-08 16:21:54 -0300

    Storing important file paths

--- model/model.go
+++ model/model.go
@@ -1,5 +1,29 @@
 package model
 
+// Just a function to kickstart stuff
 func GetTrue() bool {
-    return false
+    return true
+}
+
+// Gets the percentile table file path
+func getPercentileFilePath() string {
+    return "C:\\Users\\cris\\Documents\\scripts\\golang\\data\\dojoday\\percentile.csv"
+}
+
+// Gets the answers table file path
+func getAnswerFilePath() string {
+    return "C:\\Users\\cris\\Documents\\scripts\\golang\\data\\dojoday\\respostas.csv"
+}
+
+// Gets the validity table file path
+func getValidityFilePath() string {
+    return "C:\\Users\\cris\\Documents\\scripts\\golang\\data\\dojoday\\validade.csv"
+}
+
+// Gets the percentile according to the age and the score obtained by the
+// subject on the test
+func GetPercentileByAgeAndScore(age, score int) int {
+    filepath := getPercentileFilePath()
+    // TODO Implement this function
+    return -1
 }
--- model/model_test.go
+++ model/model_test.go
@@ -10,3 +10,20 @@ func TestCanActuallyTest(t *testing.T) {
         t.Error("true is not true? wtf??")
     }
 }
+
+func TestIfPercentileCanBeExtracted(t *testing.T) {
+    percentile := GetPercentileByAgeAndScore(5, 20)
+    if percentile != 90 {
+        t.Error("Couldn't get percentile from a 5yo that got 20 points")
+    }
+
+    percentile = GetPercentileByAgeAndScore(8, 22)
+    if percentile != 50 {
+        t.Error("Couldn't get percentile from a 8yo that got 22 points")
+    }
+
+    percentile = GetPercentileByAgeAndScore(11, 16)
+    if percentile != 0 {
+        t.Error("Couldn't get percentile from a 11yo that got 16 points")
+    }
+}
