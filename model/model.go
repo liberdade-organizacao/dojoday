5b370d356c369ea51ebf8a03ea259e10e706bd76
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-08-08 20:02:52 -0300

    Hiding information that might be sensitive later

--- model/model.go
+++ model/model.go
@@ -1,29 +1,42 @@
 package model
 
+import (
+    "os"
+)
+
 // Just a function to kickstart stuff
 func GetTrue() bool {
     return true
 }
 
+func GetSourceFolder() string {
+    return os.Getenv("DOJODAY_FILES")
+}
+
 // Gets the percentile table file path
 func getPercentileFilePath() string {
-    return "C:\\Users\\cris\\Documents\\scripts\\golang\\data\\dojoday\\percentile.csv"
+    return GetSourceFolder() + "percentile.csv"
 }
 
 // Gets the answers table file path
 func getAnswerFilePath() string {
-    return "C:\\Users\\cris\\Documents\\scripts\\golang\\data\\dojoday\\respostas.csv"
+    return GetSourceFolder() + "respostas.csv"
 }
 
 // Gets the validity table file path
 func getValidityFilePath() string {
-    return "C:\\Users\\cris\\Documents\\scripts\\golang\\data\\dojoday\\validade.csv"
+    return GetSourceFolder() + "validade.csv"
 }
 
-// Gets the percentile according to the age and the score obtained by the
-// subject on the test
-func GetPercentileByAgeAndScore(age, score int) int {
-    filepath := getPercentileFilePath()
+// Gets the percentile list
+func GetPercentile() []int {
+    outlet := make([]int, 0)
     // TODO Implement this function
-    return -1
+    return outlet
+}
+
+// TODO Get minimum score for a percentile based on the age
+func GetPercentileScoreByAge(age int) []int {
+    outlet := make([]int, 0)
+    return outlet
 }
