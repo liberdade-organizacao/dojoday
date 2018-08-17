338028f8531b283510c68db0798c89cca5e0ad58
Cris Silva Jr <ishiikurisu@yahoo.com>
2018-08-17 20:27:26 -0300

    Implemented basic state machine on view layer

---
+++ controller/controller.go
@@ -0,0 +1,15 @@
+package controller
+
+func ValidateAge(age int) bool {
+    // TODO Implement me!
+    return true
+}
+
+func ValidateAnswer(item, answer int) bool {
+    // TODO Implement me!
+    return true
+}
+
+func GetNumberOfItems() int {
+    return 3
+}
--- view/view.go
+++ view/view.go
@@ -1,9 +1,16 @@
 package view
 
+import (
+    "strconv"
+    "strings"
+    "github.com/liberdade-organizacao/dojoday/controller"
+)
+
 type StateEnum int
 const (
     AgeQuestionState StateEnum = 1 << iota
     TestState
+    EndingState
 )
 
 type View struct {
@@ -39,10 +46,47 @@ func (view *View) Read(line string) {
     // TODO Implement the rest of me!
     switch view.State {
     case AgeQuestionState:
-        view.State = TestState
-        view.Output = append(view.Output, "Digite qualquer tecla para sair")
+        age, oops := strconv.Atoi(strings.Trim(line, "\r\n"))
+        if oops != nil {
+            view.Output = append(view.Output, "Digite um valor válido!")
+        } else if controller.ValidateAge(age) {
+            view.Age = age
+            view.State = TestState
+            view.CurrentItem = 0
+            view.Output = append(view.Output, "Item A1:")
+        } else {
+            view.Output = append(view.Output, "Digite uma idade válida")
+        }
         break
     case TestState:
-        view.Working = false
+        answer, oops := strconv.Atoi(strings.Trim(line, "\r\n"))
+        if oops != nil {
+            view.Output = append(view.Output, "Digite um valor válido!")
+        } else if controller.ValidateAnswer(view.CurrentItem, answer) {
+            // TODO Store answers
+            view.CurrentItem += 1
+            if view.CurrentItem >= controller.GetNumberOfItems() {
+                view.State = EndingState
+                // TODO Display end result
+                view.Output = append(view.Output, "Deseja tentar novamente? [s/N]")
+            } else {
+                // TODO Load new items
+                view.Output = append(view.Output, "Mais uma questão...")
+            }
+        } else {
+            view.Output = append(view.Output, "Digite uma idade válida")
+        }
+        break
+    case EndingState:
+        endMe := true
+        if len(line) > 0 {
+            if (line[0] == 's') || (line[0] == 'S') {
+                view.State = TestState
+                view.CurrentItem = 0
+                view.Output = append(view.Output, "Item A1:")
+                endMe = false
+            }
+        }
+        view.Working = !endMe
     }
 }
